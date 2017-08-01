326cd3be9156812bc85c45c1ec686d5b6a2a744d
Ronaldo Hornburg <ronaldo.hornburg@embraco.com>
2017-08-01 17:19:34 -0300

    Fix main routine and eliminate redundant readme file

--- hello-world.c
+++ hello-world.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char const *argv[]) {
    int i;
-   
+
    printf("Oi, Mundo!\n\n");
-   
+
    /* Loops and prints the parameters passed */
    printf("argc: %d\n", argc);
-   
+
    for (i=0; i<argc; i++) {
       printf("argv[%d] valor: %s\n", i, argv[i]);
       printf("argv[%d] endereço: %d\n", i, *argv[i]);
-   }      
-   
+   }
+
    return 0;
 }
