e9ecd84162c42b1877851941ef0829022fcea6ee
Ronaldo Hornburg <ronaldo.hornburg@embraco.com>
2019-07-26 14:26:51 -0300

    Translated messages to english

--- hello-world.c
+++ hello-world.c
@@ -3,14 +3,14 @@
 int main(int argc, char const *argv[]) {
    int i;
 
-   printf("Oi, Mundo!\n\n");
+   printf("Hello, world!\n\n");
 
    /* Loops and prints the parameters passed */
    printf("argc: %d\n", argc);
 
    for (i=0; i<argc; i++) {
-      printf("argv[%d] valor: %s\n", i, argv[i]);
-      printf("argv[%d] endereço: %d\n", i, *argv[i]);
+      printf("argv[%d] value: %s\n", i, argv[i]);
+      printf("argv[%d] address: %d\n", i, *argv[i]);
    }
 
    return 0;
